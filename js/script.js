ad8768bbdf66d97f22b3dfeda04bf04a71692ffa
Arminiusb988 <63994703+Arminiusb988@users.noreply.github.com>
2020-05-26 21:35:58 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,62 @@
-var b1 = document.getElementById("button");
+//-----------Einleitung-----------//
+function changeHTMLEinleitung() {
+    //----------erstelle Elemente------------//	
+    //erstelle Body, da der aktuelle ersetzt wird
+    var newMain = document.createElement("div");
+    logo = new Image; //erzeuge Image inkl. Größe
+    var buttonGame = document.createElement("input");
+    var footer = document.createElement("footer");
+    var span = document.createElement("span");
 
-b1.onclick = function () {
-    window.alert("Der Button wurde gedrückt!");
-} 
+    //------------Klassen--------------//
+    logo.className = "mb-4";
+    buttonGame.className = "btn btn-lg btn-block";
+    footer.className = "footer text-center fixed-bottom p-3";
+
+    //---------weitere Eigenschaften & Bezeichnungen------------//
+    newMain.id = "newMain";
+    logo.src = "/img/Logo_Planspiel.png";
+    buttonGame.type = "button";
+    buttonGame.value = "Planspiel starten";
+    span.textContent = "Made by Diana Quaschni and Benjamin Lehnert © 2020";
+
+    //----------Zuweisungen für Anzeige-----------// 
+    newMain.appendChild(logo);
+    newMain.appendChild(buttonGame);
+    footer.appendChild(span);
+    newMain.appendChild(footer);
+    //lege Ausgabebereich fest und zeige neuen Main-Inhalt dafür an
+	var Ausgabebereich = document.getElementById("main");
+    Ausgabebereich.parentNode.replaceChild(newMain, Ausgabebereich);
+    
+
+    buttonGame.onclick = function () {
+        //-------- Daten aus der Datenbank holen ---------/
+        //Incident Daten
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "../db.php?data=incidents", true);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {  
+                var incidents = JSON.parse(xhr.responseText);
+                //alert(incidents[0].title);
+            }
+        }
+        changeHTMLGame();
+    }
+}//ENDE Einleitung
+
+//wird DOM geladen, tausche das HTML aus
+document.addEventListener('DOMContentLoaded', changeHTMLEinleitung);
+
+//-----------Hauptspiel-----------//
+function changeHTMLGame() {
+    $.get('index.html', function (data) {
+		$('body').html(data);
+    })
+}//ENDE Game
+
+//-----------Auswertung-----------//
+function changeHTMLAuswertung() {
+
+}//ENDE Auswertung
